f00db389019f868c7cbda3eaab33b6109bc4def4
fanaardie25 <follgrowthindonesia@gmail.com>
2026-02-21 14:14:59 +0700

    feat: adding Product and category resource

--- app/Filament/Resources/Categories/CategoryResource.php
+++ app/Filament/Resources/Categories/CategoryResource.php
@@ -1,46 +1,61 @@
 <?php
 
-namespace App\Filament\Resources\Products\Tables;
+namespace App\Filament\Resources\Categories;
 
-use App\Models\Product;
+use App\Filament\Resources\Categories\Pages\ManageCategories;
+use App\Models\Category;
+use BackedEnum;
 use Filament\Actions\Action;
 use Filament\Actions\BulkAction;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Support\Collection;
 
-class ProductsTable
+class CategoryResource extends Resource
 {
-    public static function configure(Table $table): Table
+    protected static ?string $model = Category::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static ?string $recordTitleAttribute = 'Category';
+
+    public static function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                Toggle::make('is_active')
+                    ->label('is Active')
+                    ->default(true),
+            ]);
+    }
+
+    public static function table(Table $table): Table
     {
         return $table
+            ->recordTitleAttribute('Category')
             ->columns([
-                TextColumn::make('name')->searchable()->sortable(),
-                TextColumn::make('category.name')->searchable()->sortable(),
-                TextColumn::make('sku')->searchable()->sortable(),
-                TextColumn::make('stock')->numeric()->sortable()
-                ->badge()
-                 ->color(fn (bool $state): string => match ($state) {
-                    false => 'danger',
-                    true => 'success',
-                })
-                ->numeric(decimalPlaces: 0),
-                TextColumn::make('price')->numeric()->sortable()
-                ->badge()
-                ->money('IDR'),
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
                 IconColumn::make('is_active')
                     ->boolean()
                     ->trueIcon('heroicon-o-check-circle')
                     ->falseIcon('heroicon-o-x-circle')
                     ->trueColor('success')
                     ->falseColor('danger')
-                    ->sortable()
-                     
+                    ->sortable(),
             ])
             ->filters([
                 //
@@ -66,5 +81,12 @@ class ProductsTable
                         })
                 ]),
             ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ManageCategories::route('/'),
+        ];
     }
 }
--- app/Filament/Resources/Products/Schemas/ProductForm.php
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -2,6 +2,9 @@
 
 namespace App\Filament\Resources\Products\Schemas;
 
+use App\Models\Category;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
 class ProductForm
@@ -10,7 +13,14 @@ class ProductForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('name')->required(),
+                Select::make('category_id')
+                    ->label('Category')
+                    ->options(Category::where('is_active',true)->pluck('name', 'id')),
+                TextInput::make('sku')->required(),
+                TextInput::make('price')->numeric()->required(),
+                TextInput::make('cost')->numeric()->required(),
+                TextInput::make('stock')->numeric()->required(),
             ]);
     }
 }
