35059037a86ff5908784b5002d311adb889f2d2f
fanaardie25 <follgrowthindonesia@gmail.com>
2026-02-22 09:27:36 +0700

    feat: adding manage user resource and police

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use App\Models\User;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')->required(),
+                TextInput::make('email')->email()->required(),
+                TextInput::make('password')->password()->required(),
+                Select::make('roles')
+                    ->relationship('roles', 'name')
+                    ->multiple()
+                    ->preload()
+                    ->searchable(),
+            ]);
+    }
+}
